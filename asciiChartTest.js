858471bab56820f465b7aac7f03b99d822f30c4d
Ezequiel Balquinta <ebalquinta.111mil@gmail.com>
2019-07-01 15:49:33 -0300

    cambios

--- asciiChartTest.js
+++ asciiChartTest.js
@@ -7,7 +7,7 @@ let s2 = new Array(120);
 // for (let i = 0; i < s2.length; i++)
 //     s2[i] = 15 * Math.sin(i * ((Math.PI * 4) / s2.length));
 
-s2[0] = Math.round(Math.random() * 15)
+s2[0] = Math.round(Math.random() * 15);
 for (i = 1; i < s2.length; i++)
-    s2[i] = s2[i - 1] + Math.round(Math.random() * (Math.random() > 0.5 ? 2 : -2))
+    s2[i] = s2[i - 1] + Math.round(Math.random() * (Math.random() > 0.5 ? 2 : -2));
 console.log(asciichart.plot(s2));
