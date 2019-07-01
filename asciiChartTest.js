24a82b0d70ee171e91d3cabe2cd97e30f12982d6
Ezequiel Balquinta <ebalquinta.111mil@gmail.com>
2019-07-01 15:45:59 -0300

    Más pruebas

--- asciiChartTest.js
+++ asciiChartTest.js
@@ -2,8 +2,12 @@ let argumentos = process.argv.slice(2);
 
 console.log(argumentos);
 
-var asciichart = require('asciichart')
-var s0 = new Array(120)
-for (var i = 0; i < s0.length; i++)
-    s0[i] = 15 * Math.sin(i * ((Math.PI * 4) / s0.length))
-console.log(asciichart.plot(s0))
+let asciichart = require('asciichart');
+let s2 = new Array(120);
+// for (let i = 0; i < s2.length; i++)
+//     s2[i] = 15 * Math.sin(i * ((Math.PI * 4) / s2.length));
+
+s2[0] = Math.round(Math.random() * 15)
+for (i = 1; i < s2.length; i++)
+    s2[i] = s2[i - 1] + Math.round(Math.random() * (Math.random() > 0.5 ? 2 : -2))
+console.log(asciichart.plot(s2));
--- asciiDataTableTest.js
+++ asciiDataTableTest.js
@@ -2,11 +2,12 @@ let AsciiTable = require('ascii-data-table').default;
 
 // or if installed by cloning git repo, use the correct path
 //var AsciiTable = require('lib/ascii-data-table').default
+let argumentos = process.argv.slice(2);
 
 let items = [
-    ['x', 'y'],
-    ['a', 'b'],
-    ['c', 'd']
+    [argumentos[0], argumentos[1]],
+    [argumentos[2], argumentos[3]],
+    [argumentos[4], argumentos[5]]
 ];
 
 let res = AsciiTable.table(items);
